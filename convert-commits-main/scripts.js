b034ea537645faafc1769c46459221281788d281
Fabio Marconi <fabiolukascj@gmail.com>
2025-10-16 22:11:53 -0400

    Criado a logica em JavaScript

--- convert-commits-main/scripts.js
+++ convert-commits-main/scripts.js
@@ -1,75 +1,61 @@
-//Cotação de moedas do dia.
+console.log("Arquivo carregado para a pagina! ")
 const USD = 4.87
-const EUR = 5.32
-const GBP = 6.08
+const EUR = 5.30
+const GBP = 6.15
 
-// Obtendo os elementos do formulário.
+// selecionando os elemento do HTML 
 const form = document.querySelector("form")
-const amount = document.getElementById("amount")
-const currency =document.getElementById("currency")
+const input = document.querySelector("#amount")
+const currency = document.querySelector("#currency")
 const footer = document.querySelector("main footer")
-const description = document.getElementById("description")
-const result = document.getElementById("result")
+const description = document.querySelector("#description")
+const result = document.querySelector("#result")
 
-// Manipulando o input amount para receber somente números.
-amount.addEventListener("input", () => {
-  const hasCharactersRegex = /\D+/g
-  amount.value = amount.value.replace(hasCharactersRegex, "")
+//pega valida valor do imput
+input.addEventListener("input", () => {
+    const regex = /\D+/g
+    console.log(input.value)
+    input.value = input.value.replace(regex, "")
 })
 
-// Captando o evento de submit (enviar) do formulário.
-form.onsubmit = (event) => {
-  event.preventDefault()
+//pega o evento do formulário
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
 
-  switch (currency.value){
-    case "USD":
-      convertCurrency(amount.value, USD, "US$")
-      break
-    case "EUR":
-      convertCurrency(amount.value, EUR, "€")
-      break
-    case "GBP":
-      convertCurrency(amount.value, GBP, "£")
-      break
-  }
-}
-
-// Função para converter a moeda.
-function convertCurrency(amount, price, symbol){
-  try {
-    // Exibindo a cotação da moeda selecionada.
-    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
-
-    // Calcula o total.
-    let total = amount * price
-
-    // Verifica se o resultado não é um número.
-    if (isNaN(total)) {
-      return alert("Por favor, digite o valor corretamente para converter.")
+    switch (currency.value) {
+        case "USD":
+            converter(input.value, USD, "US$")
+            break
+        case "EUR":
+            converter(input.value, EUR, "€")
+            break
+        case "GBP":
+            converter(input.value, GBP, "£")
+            break
     }
+})
 
-    // Formatar o valor total.
-    total = formatCurrencyBRL(total).replace("R$", "")
+//Função que converte o valor da Moeda.
+function converter(input, currency, simbol) {
+    try {
+        description.textContent = `${simbol} 1 = ${formatarMoedaBR(currency)}`
+        
+        let total = input * currency
 
-    // Exibe o resultado total.
-    result.textContent = `${total} Reais`
-
-    // Aplica a classe que exibe o footer para mostrar o resultado.
-    footer.classList.add("show-result")
-  } catch {
-    // Remove a classe do footer removendo ele da tela.
-    footer.classList.remove("show-result")
-    
-    console.log(error)
-    alert("Não foi possível converter. Tente novamente mais tarde.")
-  }
+        result.textContent = `${formatarMoedaBR(total)}`
+        footer.classList.add("show-result")
+        
+    } catch (error) {
+        console.log(error)
+        footer.classList.remove("show-result")
+        alert("Erro ao tentar converter a moeda, tente novamente mais tarde!")
+    }
 }
 
-// Formata a moeda em Real Brasileiro.
-function formatCurrencyBRL(value) {
-  // Converte para número para utilizar o toLocaleString para formatar no padrão BRL (R$ 00,00).
-  return Number(value).toLocaleString("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  })
+// formata o valor em reais BR
+function formatarMoedaBR(valor){
+    return Number(valor).toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL",
+    })
 }
